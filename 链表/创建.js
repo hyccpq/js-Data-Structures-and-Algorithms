afb502e43d595a646e16b5f19f971f031eede690
hyccpq <hyccpq@hotmail.com>
2017-10-22 16:17:29 +0800

    链表+添加

--- 链表/创建.js
+++ 链表/创建.js
@@ -46,11 +46,83 @@ function LinkedList() {
         else {
             return null;
         }
+    };
+
+    this.insert = function (position,element) {
+        if(position>=0&&position<=length){
+
+            let node = new Node(element),
+                current = head,
+                previous,
+                index = 0;
+
+            if(position === 0){
+                node.next = current;
+                head = node;
+            }
+            else {
+                while(index++<position){
+                    previous = current;
+                    current = current.next;
+                }
+                node.next = current;
+                previous.next = node;
+            }
+            length++;
+
+            return true;
+
+        }
+        else {
+            return false;
+        }
+    };
+
+    this.toString = function() {
+        let current = head,
+            string = '';
+        while(current){
+            string += current.element+(current.next?'-':'');
+            current = current.next;
+        }
+        return string;
+    };
+    
+    this.indexOf = function (element) {
+        let current = head,
+            index = 1;
+        while(current){
+            if(current.element == element){
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+        return -1;
+    };
+
+    this.size = function () {
+        return length;
+    };
+
+    this.isEmpty = function(){
+        return length === 0;
+    };
+
+    this.getHead = function () {
+        return head;
     }
-
-
-
 }
 
-
-
+let linkedList = new LinkedList();
+linkedList.append('23');
+linkedList.append('34');
+linkedList.append('788');
+linkedList.append('666');
+console.log('打印链表：'+linkedList.toString());
+linkedList.insert(3,'abc');
+console.log('在第3个位置插入abc：'+linkedList.toString());
+linkedList.removeAt(2);
+console.log('移除第二的元素：'+linkedList.toString());
+console.log('查找abc在：'+linkedList.indexOf('abc'));
+console.log(`链表的长度为：${linkedList.size()}`);
